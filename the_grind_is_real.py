cf869172bcf11321cbfb3de655cc717dc5ead68e
John Lenin <chinchaoth@gmail.com>
2019-01-24 13:13:20 +0700

    Update the_grind_is_real.py

--- the_grind_is_real.py
+++ the_grind_is_real.py
@@ -8,12 +8,13 @@ driver = webdriver.Chrome(chrome_options=options)
 driver.get('https://readtheory.org/auth/login')
 
 username = driver.find_element_by_xpath('//*[@id="username"]')
-username.send_keys('fake@email')
+username.send_keys('14367@students.isb.ac.th')
 
 password = driver.find_element_by_xpath('//*[@id="password"]')
-password.send_keys('fake_password')
+password.send_keys('mhNF2PqdfjF3W')
 
 login_click = driver.find_element_by_xpath('//*[@id="ajaxLogin"]').click()
+
 
 answer_click = driver.find_element_by_xpath('//*[@class="answer-label"]').click()
 
@@ -22,8 +23,14 @@ submit_click = driver.find_element_by_xpath('//*[@id="presentQuizForm-answerQues
 next_quest = driver.find_element_by_xpath('//*[@id="presentQuizForm-nextQuestionButton"]').click()
 
 
+
+
 # //*[@id="presentQuizForm-button"]
 
 # Finish and Score
 
+#https://medium.com/@stevennatera/web-scraping-with-selenium-and-chrome-canary-on-macos-fc2eff723f9e
+
 # <div id="nextQuiz-button" class="jquery-button nextQuiz-link">Next Quiz</div>
+
+
